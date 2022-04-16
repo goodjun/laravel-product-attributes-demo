257c0a95423ecf0428f005b67f9c1f8431e9638d
goodjun <goodjun@163.com>
2022-04-16 14:58:58 +0800

    feat: 创建model、migration文件

---
+++ database/factories/AttributeOptionFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\AttributeOption::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/migrations/2022_04_16_065432_create_attribute_options_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAttributeOptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attribute_options', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attribute_options');
+    }
+}
